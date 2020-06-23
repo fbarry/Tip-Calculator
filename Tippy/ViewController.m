c1ff616f03fa48ad1624858a53307db2b7e5c6e7
Fiona Barry <fbarry@fb.com>
2020-06-23 18:36:34 -0400

    Created user interface

--- Tippy/ViewController.m
+++ Tippy/ViewController.m
@@ -10,6 +10,11 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipController;
+
 @end
 
 @implementation ViewController
@@ -19,5 +24,20 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    NSArray *tipAmounts = @[@(0.15), @(0.2), @(0.22)];
+    double tipPercentage = [tipAmounts[self.tipController.selectedSegmentIndex] doubleValue];
+    
+    double bill = [self.billField.text doubleValue];
+    double tip = tipPercentage * bill;
+    double total = bill + tip;
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
